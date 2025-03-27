647bb2f1a7a924bc561ca16329c378805187340f
codingbecca <131915898+codingbecca@users.noreply.github.com>
2025-03-27 12:53:57 -0700

    updated to call deployed backend api

--- src/api/pattern.js
+++ src/api/pattern.js
@@ -1,33 +1,35 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+
 // create a new pattern
 export async function createPattern(patternDetails){
-    const res = await axios.post("/api/patterns", patternDetails);
+    const res = await axios.post(`${BASE_URL}/api/patterns`, patternDetails);
 
     return res.data
 }
 
 // get all patterns
 export async function getPatterns(){
-    const res = await axios.get('/api/patterns');
+    const res = await axios.get(`${BASE_URL}/api/patterns`);
 
     return res.data
 }
 
 // get a specific pattern
 export async function getPattern(patternId) {
-    const res = await axios.get(`/api/patterns/${patternId}`)
+    const res = await axios.get(`${BASE_URL}/api/patterns/${patternId}`)
     return res.data
 }
 
 // update a pattern
 export async function editPattern(patternId, patternDetails) {
-    const res = await axios.patch(`/api/patterns/${patternId}`, patternDetails)
+    const res = await axios.patch(`${BASE_URL}/api/patterns/${patternId}`, patternDetails)
 
     return res.data
 }
 
 // delete a pattern
 export async function deletePattern(patternId) {
-    await axios.delete(`/api/patterns/${patternId}`)
+    await axios.delete(`${BASE_URL}/api/patterns/${patternId}`)
 }
--- src/api/project.js
+++ src/api/project.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+
 // create a new project
 export async function createProject(projectInfo){ 
-    const res = await axios.post("/api/projects", projectInfo)
+    const res = await axios.post(`${BASE_URL}/api/projects`, projectInfo)
 
     return res.data
 }
 
 // get all projects
 export async function getProjects() {
-    const res = await axios.get('/api/projects')
+    const res = await axios.get(`${BASE_URL}/api/projects`)
 
     return res.data
 }
